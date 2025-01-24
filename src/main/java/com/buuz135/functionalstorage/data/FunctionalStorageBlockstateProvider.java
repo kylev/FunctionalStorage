62c739554b08ed208cccd885f4d417f34cc2108a
Kyle VanderBeek <kylev@kylev.com>
2025-01-24 11:24:31 -0800

    Simpler but incompatible generator loop.

--- src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageBlockstateProvider.java
+++ src/main/java/com/buuz135/functionalstorage/data/FunctionalStorageBlockstateProvider.java
@@ -1,28 +1,26 @@
 package com.buuz135.functionalstorage.data;
 
 import com.buuz135.functionalstorage.FunctionalStorage;
-import com.buuz135.functionalstorage.block.CompactingDrawerBlock;
 import com.buuz135.functionalstorage.block.DrawerBlock;
-import com.buuz135.functionalstorage.block.EnderDrawerBlock;
-import com.buuz135.functionalstorage.block.FluidDrawerBlock;
-import com.buuz135.functionalstorage.block.SimpleCompactingDrawerBlock;
 import com.hrznstudio.titanium.block.RotatableBlock;
+
+import com.mojang.logging.LogUtils;
 import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.client.model.generators.ConfiguredModel;
 import net.neoforged.neoforge.client.model.generators.ModelFile;
-import net.neoforged.neoforge.client.model.generators.VariantBlockStateBuilder;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.common.util.Lazy;
 
 import java.util.List;
 
 public class FunctionalStorageBlockstateProvider extends BlockStateProvider {
+
+    private static final org.slf4j.Logger LOGGER = LogUtils.getLogger();
     private final Lazy<List<Block>> blocks;
 
     public FunctionalStorageBlockstateProvider(DataGenerator gen, ExistingFileHelper exFileHelper, Lazy<List<Block>> blocks) {
@@ -30,37 +28,33 @@ public class FunctionalStorageBlockstateProvider extends BlockStateProvider {
         this.blocks = blocks;
     }
 
-    public static ResourceLocation getModel(Block block) {
-        return com.buuz135.functionalstorage.util.Utils.resourceLocation(BuiltInRegistries.BLOCK.getKey(block).getNamespace(), "block/" + BuiltInRegistries.BLOCK.getKey(block).getPath());
-    }
-
-    public static ResourceLocation getModelLocked(Block block) {
-        return com.buuz135.functionalstorage.util.Utils.resourceLocation(BuiltInRegistries.BLOCK.getKey(block).getNamespace(), "block/" + BuiltInRegistries.BLOCK.getKey(block).getPath() + "_locked");
+    public static ResourceLocation getModel(Block block, boolean locked) {
+        return com.buuz135.functionalstorage.util.Utils.resourceLocation(BuiltInRegistries.BLOCK.getKey(block).getNamespace(),
+                "block/" + BuiltInRegistries.BLOCK.getKey(block).getPath() + (locked ? "_locked" : ""));
     }
 
     @Override
     protected void registerStatesAndModels() {
-        blocks.get().stream().filter(blockBase -> blockBase instanceof RotatableBlock)
-                .map(blockBase -> (RotatableBlock) blockBase)
-                .forEach(rotatableBlock -> {
-                    VariantBlockStateBuilder builder = getVariantBuilder(rotatableBlock);
-                    if (rotatableBlock.getRotationType().getProperties().length > 0) {
-                        for (DirectionProperty property : rotatableBlock.getRotationType().getProperties()) {
-                            for (Direction allowedValue : property.getPossibleValues()) {
-                                if (rotatableBlock instanceof DrawerBlock || rotatableBlock instanceof CompactingDrawerBlock || rotatableBlock instanceof EnderDrawerBlock || rotatableBlock instanceof FluidDrawerBlock || rotatableBlock instanceof SimpleCompactingDrawerBlock) {
-                                    builder.partialState().with(property, allowedValue).with(DrawerBlock.LOCKED, false)
-                                            .addModels(new ConfiguredModel(new ModelFile.UncheckedModelFile(getModel(rotatableBlock)), allowedValue.get2DDataValue() == -1 ? allowedValue.getOpposite().getAxisDirection().getStep() * 90 : 0, (int) allowedValue.getOpposite().toYRot(), true));
-                                    builder.partialState().with(property, allowedValue).with(DrawerBlock.LOCKED, true)
-                                            .addModels(new ConfiguredModel(new ModelFile.UncheckedModelFile(getModelLocked(rotatableBlock)), allowedValue.get2DDataValue() == -1 ? allowedValue.getOpposite().getAxisDirection().getStep() * 90 : 0, (int) allowedValue.getOpposite().toYRot(), true));
-                                } else {
-                                    builder.partialState().with(property, allowedValue)
-                                            .addModels(new ConfiguredModel(new ModelFile.UncheckedModelFile(getModel(rotatableBlock)), allowedValue.get2DDataValue() == -1 ? allowedValue.getOpposite().getAxisDirection().getStep() * 90 : 0, (int) allowedValue.getOpposite().toYRot(), true));
-                                }
-                            }
-                        }
-                    } else {
-                        builder.partialState().addModels(new ConfiguredModel(new ModelFile.UncheckedModelFile(getModel(rotatableBlock))));
-                    }
-                });
+        blocks.get().stream()
+            .filter(b -> b instanceof RotatableBlock)
+            .forEach(b -> registerForAllLoop((RotatableBlock) b));
+    }
+
+    private void registerForAllLoop(RotatableBlock<?> block) {
+        getVariantBuilder(block).forAllStates(state -> {
+            boolean locked = false;
+            Direction facing = state.getValue(RotatableBlock.FACING_HORIZONTAL);
+            if (block instanceof DrawerBlock) {
+                LOGGER.info("DrawerBlock {}", state);
+                locked = state.getValue(DrawerBlock.LOCKED);
+            }
+
+            var modelFile = new ModelFile.UncheckedModelFile(getModel(block, locked));
+            return ConfiguredModel.builder()
+                    .modelFile(modelFile)
+                    .rotationY((int) facing.getOpposite().toYRot())
+                    .uvLock(true)
+                    .build();
+        });
     }
 }
